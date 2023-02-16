e947f0111208d6fa1c578a52139e469ea512cef1
jl <jeanlouis.macle@gmail.com>
2023-02-16 14:57:45 -0600

    Script for Linux

--- cypress/e2e/run-to-find-the-keywords-in-the-websites.sh
+++ cypress/e2e/run-to-find-the-keywords-in-the-websites.sh
@@ -6,18 +6,19 @@ echo "Re-run the script if no test is generated."
 echo "Note: You might have false positives,"
 echo "that is, a screenshot is taken without the keyword being on the picture."
 echo "That being said, the program does find the keywords in some of the pages."
-echo "**************************************************************************
-sleep 120
+echo "**************************************************************************"
 
 echo "Deleting previous specs screenshots"
 rm -f "../screenshots/$generated-file-name/*.*" 2>nul
 rm $generated-file-name 2>nul
-sleep 300
 
 echo "Starting the tests generation"
 node kw-in-news-spec-generator.js
-echo "a TimeOut value in case of test generation issue.""
+
+echo "a 10s TimeOut value in case of test generation issue."
 
 sleep 10
 
-start "Tests run by Cypress" spec-run.sh
+cd ../..
+npx cypress run --config screenshotOnRunFailure=false,video=false --spec ./cypress/e2e/$generated-file-name
+cd cypress/e2e
