339250985cbeb7fc75a0cd80f01d57a5769495db
Jean-Louis <jeanlouis.macle@gmail.com>
2023-02-16 23:36:29 +0100

    Readme file

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # News monitoring helper
-<<<<<<< HEAD
 News monitoring program based on Cypress, potentially useful to find keywords in news pages.
-=======
-News monitoring program based on Cypress, and potentially useful to find keywords in news pages.
->>>>>>> 96ce35a (Running version of the news monitoring helper)
 
 
---
+++ cypress/e2e/run-to-find-the-keywords-in-the-websites.sh
@@ -0,0 +1,24 @@
+set generatedFileName="Generated-File_keywords-in-websites.cy.js"
+clear
+echo "**************************************************************************"
+echo "Please do not mind the randomly generated order of the tests."
+echo "Re-run the script if no test is generated."
+echo "Note: You might have false positives,"
+echo "that is, a screenshot is taken without the keyword being on the picture."
+echo "That being said, the program does find the keywords in some of the pages."
+echo "**************************************************************************
+
+echo "Deleting previous specs screenshots"
+rm -f "../screenshots/$generatedFileName/*.*" 2>nul
+rm $generatedFileName 2>nul
+
+echo "Starting the tests generation"
+node kw-in-news-spec-generator.js
+
+echo "a 10s TimeOut value in case of test generation issue.""
+
+sleep 10
+
+cd ../..
+npx cypress run --config screenshotOnRunFailure=false,video=false --spec ./cypress/e2e/$generatedFileName
+cd cypress/e2e
