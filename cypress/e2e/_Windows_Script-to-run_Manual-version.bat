c3484a28aaf9f7e0399f65fa9493c7cb4a539313
jl <jeanlouis.macle@gmail.com>
2023-02-24 15:17:15 -0600

    Script for close to fail-free version - manual version

--- cypress/e2e/_Windows_Script-to-run_Manual-version.bat
+++ cypress/e2e/_Windows_Script-to-run_Manual-version.bat
@@ -1,6 +1,8 @@
+SETLOCAL ENABLEDELAYEDEXPANSION
 set generated-file-name=Generated-File_keywords-in-websites.cy.js
 set debug=1
 
+cls
 echo off
 
 echo.
@@ -14,63 +16,46 @@ echo.
 echo.
 
 set NUMBER_OF_LINES_IN_GENERATED_FILE=0
-FOR /L %%v IN (1 1 5 ) DO  (
-    echo %%v
-)
-echo before loop
-FOR /L %%v IN ( 1 1 10 ) DO  
-(
-    echo loop
+
+FOR /L %%v IN ( 1 1 10 ) DO  (   
+    echo.
+    echo.
+    echo %%v 
     echo.
     @REM Lines to count in the generated file
     @REM if less than 7 lines are generated, the test generation has failed 
-    
-    
 
-    @REM if %NUMBER_OF_LINES_IN_GENERATED_FILE% LSS 7 
-    @REM (
-    @REM     echo Deleting the previously generated tests (Generated-File_keywords-in-websites.cy.js)
-    @REM     del %generated-file-name% 
-    @REM     if %debug% equ 1 ( dir )
-    @REM     if %debug% equ 1 ( pause )
-    @REM     echo.
-    @REM     echo.
-    @REM     echo Starting the tests generation
-    @REM     node kw-in-news-spec-generator.js
-    @REM     echo Number of lines in the generated file
-    @REM     find /c /v "" Generated-File_keywords-in-websites.cy.js
-    @REM     for /f "usebackq delims==" %%i in (`find /c /v "" Generated-File_keywords-in-websites.cy.js`) do set command-output=%%i  
-    @REM     @REM "---------- GENERATED-FILE_KEYWORDS-IN-WEBSITES.CY.JS: "
-    @REM     @REM  has 54 characters
-    @REM     SET NUMBER_OF_LINES_IN_GENERATED_FILE=%command-output:~54,-2%
-    @REM     echo %command-output:~54,-2%
-    @REM     ECHO *%NUMBER_OF_LINES_IN_GENERATED_FILE%* lines in the generated file
+    if !NUMBER_OF_LINES_IN_GENERATED_FILE! LSS 7 (
+        echo !NUMBER_OF_LINES_IN_GENERATED_FILE! is less than 7
+        echo Deleting the previously generated tests (Generated-File_keywords-in-websites.cy.js)
+        del !generated-file-name! 
+        if !debug! equ 1 ( dir )
+        if !debug! equ 1 ( pause )
+        echo.
+        echo.
+        echo Starting the tests generation
+        node kw-in-news-spec-generator.js
+        echo Number of lines in the generated file
+        find /c /v "" Generated-File_keywords-in-websites.cy.js
+        for /f "usebackq delims==" %%i in (`find /c /v "" Generated-File_keywords-in-websites.cy.js`) do set command-output=%%i  
+        @REM "---------- GENERATED-FILE_KEYWORDS-IN-WEBSITES.CY.JS: "
+        @REM  has 54 characters
+        set NUMBER_OF_LINES_IN_GENERATED_FILE=!command-output:~54,-2!
+        echo *!NUMBER_OF_LINES_IN_GENERATED_FILE!* lines in the generated file
 
-    @REM     pause
-    @REM ) 
-    @REM else 
-    @REM (
-    @REM     echo.
-    @REM     echo.
-    @REM     echo The test generation has been successful.
-    @REM     echo.
-    @REM     echo.
-    @REM     echo Starting the tests run
-    @REM     call spec-run.bat
-    @REM     pause
-    @REM     exit
-    @REM )
+    ) 
+    else (
+        echo.
+        echo.
+        echo The test generation has been successful.
+        echo.
+        echo.
+        echo Starting the tests run
+        call spec-run.bat
+        pause
+        exit
+    )
 )
-
-@REM echo Number of lines in the generated file
-    @REM find /c /v "" Generated-File_keywords-in-websites.cy.js
-    @REM for /f "usebackq delims==" %%i in (`find /c /v "" Generated-File_keywords-in-websites.cy.js`) do set command-output=%%i  
-    @REM @REM "---------- GENERATED-FILE_KEYWORDS-IN-WEBSITES.CY.JS: "
-    @REM @REM  has 54 characters
-    @REM SET NUMBER_OF_LINES_IN_GENERATED_FILE=%command-output:~54,-2%
-
-    @REM @REM NUMBER_OF_LINES_IN_GENERATED_FILE=%NUMBER_OF_LINES_IN_GENERATED_FILE:~0,3%
-    @REM ECHO *%NUMBER_OF_LINES_IN_GENERATED_FILE%* lines in the generated file
 
 :: Credit to https://www.delftstack.com/howto/batch/batch-for-loop/?utm_content=cmp-true
 :: for the FOR /L command
